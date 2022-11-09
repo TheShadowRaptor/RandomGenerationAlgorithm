43c0ff3c37afe51c0bd87157b1324f1529ea442e
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2022-11-09 17:04:13 -0400

    [ADDED] GradualHeightSmoothing

--- Assets/GenerateCubes.cs
+++ Assets/GenerateCubes.cs
@@ -17,6 +17,11 @@ public class GenerateCubes : MonoBehaviour
     public int emptySpaceWeight = 2;
     private int emptySpaceAmount;
 
+    [Header("Customization")]
+    public bool smoothHeightToCenter = true;
+    public bool smoothHeightToBorder = true;
+    public bool smoothHeightToInnerComplex = true;
+
     [Header("PerlinNoise")]
     [Range(0, 10)][SerializeField] float amplitude = 5.0f;
     [Range(0, 10)][SerializeField] float frequency = 5.0f;
@@ -49,6 +54,8 @@ public class GenerateCubes : MonoBehaviour
 
                     Vector3 localPosition = new Vector3(x, 0, z);
                     int currentBlockHeight = Random.Range(blockMinPosY, blockMaxPosY);
+
+                    // Will fill space or leave it blank
                     if (emptySpaceAmount > 0 || x < mapBorderSize || z < mapBorderSize ||
                     x > blockMaxPosX - mapBorderSize || z > blockMaxPosZ - mapBorderSize)
                     {
@@ -60,7 +67,7 @@ public class GenerateCubes : MonoBehaviour
 
                     else
                     {
-                        for (int y = 0; y <= currentBlockHeight; y++)
+                        for (int y = 0; y <= GradualHeightSmoothing(currentBlockHeight, x, z); y++)
                         {
                             localPosition.y = y;
                             Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -75,8 +82,87 @@ public class GenerateCubes : MonoBehaviour
         }
     }
 
-    void SmoothHeight()
+    float GradualHeightSmoothing(float currentBlockHeight, int x, int z)
     {
+        int currentBlockPosX = x;
+        int currentBlockPosZ = z;
 
+        int blockMiddlePosX = blockMaxPosX / 2;
+        int blockMiddlePosZ = blockMaxPosZ / 2;
+
+        if (smoothHeightToCenter)
+        {
+
+            if (currentBlockPosX < blockMiddlePosX / 3 || currentBlockPosZ < blockMiddlePosZ / 3 ||
+                currentBlockPosX > blockMiddlePosX * 3.5f / 2 || currentBlockPosZ > blockMiddlePosZ * 3.5f / 2)
+            {
+                currentBlockHeight /= 3;
+            }
+
+            else if (currentBlockPosX < blockMiddlePosX / 2 || currentBlockPosZ < blockMiddlePosZ / 2 ||
+                currentBlockPosX > blockMiddlePosX * 3 / 2 || currentBlockPosZ > blockMiddlePosZ * 3 / 2)
+            {
+                currentBlockHeight /= 2;
+            }
+
+            else if (currentBlockPosX < blockMiddlePosX / 1.5f || currentBlockPosZ < blockMiddlePosZ / 1.5f ||
+               currentBlockPosX > blockMiddlePosX * 2.5f / 2 || currentBlockPosZ > blockMiddlePosZ * 2.5f / 2)
+            {
+                currentBlockHeight /= 1.5f;
+            }
+        }
+
+        if (smoothHeightToBorder)
+        {
+            if (currentBlockPosX < blockMiddlePosX / 3 || currentBlockPosZ < blockMiddlePosZ / 3 ||
+                currentBlockPosX > blockMiddlePosX * 3.5f / 2 || currentBlockPosZ > blockMiddlePosZ * 3.5f / 2)
+            {
+                return currentBlockHeight;
+            }
+
+            else if (currentBlockPosX < blockMiddlePosX / 2 || currentBlockPosZ < blockMiddlePosZ / 2 ||
+                currentBlockPosX > blockMiddlePosX * 3 / 2 || currentBlockPosZ > blockMiddlePosZ * 3 / 2)
+            {
+                currentBlockHeight /= 1.5f;
+            }
+
+            else if (currentBlockPosX < blockMiddlePosX / 1.1f || currentBlockPosZ < blockMiddlePosZ / 1.1f ||
+               currentBlockPosX > blockMiddlePosX * 2.5f / 2 || currentBlockPosZ > blockMiddlePosZ * 2.5f / 2)
+            {
+                currentBlockHeight /= 4;
+            }
+            else if (currentBlockPosX > blockMiddlePosX / 1.1f || currentBlockPosZ > blockMiddlePosZ / 1.1f ||
+               currentBlockPosX < blockMiddlePosX * 2.5f / 2 || currentBlockPosZ < blockMiddlePosZ * 2.5f / 2)
+            {
+                currentBlockHeight /= 4;
+            }
+        }
+
+        if (smoothHeightToInnerComplex)
+        {
+            if (currentBlockPosX < blockMiddlePosX / 3 || currentBlockPosZ < blockMiddlePosZ / 3 ||
+               currentBlockPosX > blockMiddlePosX * 3.5f / 2 || currentBlockPosZ > blockMiddlePosZ * 3.5f / 2)
+            {
+                return currentBlockHeight;
+            }
+
+            else if (currentBlockPosX < blockMiddlePosX / 2 || currentBlockPosZ < blockMiddlePosZ / 2 ||
+                currentBlockPosX > blockMiddlePosX * 3 / 2 || currentBlockPosZ > blockMiddlePosZ * 3 / 2)
+            {
+                currentBlockHeight /= 0.5f;
+            }
+
+            else if (currentBlockPosX < blockMiddlePosX / 1.5f || currentBlockPosZ < blockMiddlePosZ / 1.5f ||
+               currentBlockPosX > blockMiddlePosX * 2.5f / 2 || currentBlockPosZ > blockMiddlePosZ * 2.5f / 2)
+            {
+                currentBlockHeight /= 3;
+            }
+            else if (currentBlockPosX > blockMiddlePosX / 1.5f || currentBlockPosZ > blockMiddlePosZ / 1.5f ||
+               currentBlockPosX < blockMiddlePosX * 2.5f / 2 || currentBlockPosZ < blockMiddlePosZ * 2.5f / 2)
+            {
+                currentBlockHeight /= 3;
+            }
+        }
+        return currentBlockHeight;
     }
 }
